49b90f3f98f1a45b6ba1a7ea5c2e0b729e814e56
= <=>
2023-02-23 09:22:51 +0200

    testing

--- controller/index.js
+++ controller/index.js
@@ -14,9 +14,15 @@ const user = new User();
 const product = new Product();
 // ^/$|/jtbookstore
 
-route.get('^/$|/jtbookstore', (req, res)=>{
-    res.status(200).sendFile(path.join(__dirname, '../view/index.html'));
+route.get('/', (req, res) => {
+    res.send({
+        message: "Welcome to this api"
+    })
 })
+
+// route.get('^/$|/jtbookstore', (req, res)=>{
+//     res.status(200).sendFile(path.join(__dirname, '../view/index.html'));
+// })
 // =========USER's Router========
 // Login
 route.post('/login', bodyParser.json(), (req, res)=>{
